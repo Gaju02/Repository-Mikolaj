b47fd30f232538e466b7d837b62fbc30620c3145
Mikołaj <84478047+Gaju02@users.noreply.github.com>
2023-08-06 11:44:55 +0200

    Delete Server_management_backend/Server_management_backend/demo directory

--- Server_management_backend/Server_management_backend/demo/src/main/java/com/example/demo/Repo/ServerRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.Repo;
-
-import com.example.demo.model.Server;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ServerRepo extends JpaRepository<Server,Long> {
-    Server findByIpAddress(String ipAddress);
-    Server findByName(String name);
-}
